79e8fb6996601de1fe82cf2a3c16d545d70dff17
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-23 12:08:30 +0430

    Initial Repo

---
+++ RecursivePowerMethod/Power.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursivePowerMethod
+{
+    public class Power
+    {
+        public int Calculate(int number, int power)
+        {
+            if (power == 0)
+                return 1;
+            else
+                return number * Calculate(number, power - 1);
+        }
+    }
+}
---
+++ RecursivePowerMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿using RecursivePowerMethod;
+
+Console.WriteLine("Please enter number : ");
+var Inputnumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter power : ");
+var Inputpower = int.Parse(Console.ReadLine());
+
+var power = new Power();
+var result = power.Calculate(Inputnumber, Inputpower);
+
+Console.WriteLine($"result is : {result}");
+
